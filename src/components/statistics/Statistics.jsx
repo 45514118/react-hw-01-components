0e419af01e758fa88837d09e4929ed6211d180ed
Deenar <mail@deenar.ru>
2022-03-17 16:57:29 +0300

    quicksave

---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,14 @@
+export const Statistics = (stats) => {
+    return (
+        <section class="statistics">
+            <h2 class="title">Upload stats</h2>
+            <ul class="stat-list">
+                {stats.map(stat => 
+                    <li class="item" id={stat.id}>
+                        <span class="label">{stat.label}</span>
+                        <span class="percentage">{stat.percentage}%</span>
+                    </li>)}
+            </ul>
+        </section>
+    )
+}
