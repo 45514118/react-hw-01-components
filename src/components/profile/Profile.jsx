3ee0d7a96d4c16b2e837a4b53423aee804c6f004
Deenar <mail@deenar.ru>
2022-03-18 17:25:39 +0300

    quicksave

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -6,19 +6,19 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       <div class={style.description}>
         <img src={avatar} alt="User avatar" class={style.avatar} />
         <p class={style.name}>{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
+        <p class={style.tag}>@{tag}</p>
+        <p class={style.location}>{location}</p>
       </div>
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
+      <ul class={style.stats}>
+        <li class={style.stats__item}>
+          <span class={style.label}>Followers</span>
           <span class="quantity">{stats.followers}</span>
         </li>
-        <li>
-          <span class="label">Views</span>
+        <li class={style.stats__item}>
+          <span class={style.label}>Views</span>
           <span class="quantity">{stats.views}</span>
         </li>
-        <li>
+        <li class={style.stats__item}>
           <span class="label">Likes</span>
           <span class="quantity">{stats.likes}</span>
         </li>
