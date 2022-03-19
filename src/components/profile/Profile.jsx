2164cd1ef9649475e774ec164123ecae0a6ee1cf
Dinar Agzyamov <mail@deenar.ru>
2022-03-19 08:14:05 +0300

    homework 1.1 done

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,25 +2,25 @@ import style from "./Profile.module.css";
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class={style.profile}>
-      <div class={style.description}>
-        <img src={avatar} alt="User avatar" class={style.avatar} />
-        <p class={style.name}>{username}</p>
-        <p class={style.tag}>@{tag}</p>
-        <p class={style.location}>{location}</p>
+    <div className={style.profile}>
+      <div className={style.description}>
+        <img src={avatar} alt="User avatar" className={style.avatar} />
+        <p className={style.name}>{username}</p>
+        <p className={style.tag}>@{tag}</p>
+        <p className={style.location}>{location}</p>
       </div>
-      <ul class={style.stats}>
-        <li class={style.stats__item}>
-          <span class={style.label}>Followers</span>
-          <span class="quantity">{stats.followers}</span>
+      <ul className={style.stats}>
+        <li className={style.stats__item}>
+          <span className={style.label}>Followers</span>
+          <span className={style.quantity}>{stats.followers}</span>
         </li>
-        <li class={style.stats__item}>
-          <span class={style.label}>Views</span>
-          <span class="quantity">{stats.views}</span>
+        <li className={style.stats__item}>
+          <span className={style.label}>Views</span>
+          <span className={style.quantity}>{stats.views}</span>
         </li>
-        <li class={style.stats__item}>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
+        <li className={style.stats__item}>
+          <span className={style.label}>Likes</span>
+          <span className={style.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
